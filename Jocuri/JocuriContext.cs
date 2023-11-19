5c6f01ea4260f888b4b6189ecb9a33f778f5b05d
alin-alexandru-toader <86752561+alin-alexandru-toader@users.noreply.github.com>
2023-11-19 20:25:12 +0200

    Add project files.

---
+++ Jocuri/JocuriContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Jocuri.Models.Entities;
+
+namespace Jocuri
+{
+    public class JocuriContext : DbContext
+    {
+        public JocuriContext(DbContextOptions<JocuriContext> options)
+            : base(options) 
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ Jocuri/Models/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace Jocuri.Models.Entities
+{
+    public class Product
+    {
+        public Product()
+        {
+            Name = string.Empty;
+            Description = string.Empty;
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public bool IsAvailable { get; set; }
+        public string? ImagePath { get; set; }
+    }
+}
